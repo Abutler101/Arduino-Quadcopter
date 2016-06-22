25d45b13470218042501e0f259324684f3f7b5a8
Abutler101 <alexbutler1810@hotmail.com>
2016-06-22 17:56:06 +0100

    added which way do you spin?

---
+++ which_Way_do_you_spin.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+Servo MotorFL;
+Servo MotorFR;
+Servo MotorBL;
+Servo MotorBR;
+void setup()
+{
+  MotorFL.attach(24);
+  MotorFR.attach(26);
+  MotorBL.attach(28);
+  MotorBR.attach(30);
+  delay(60000);
+  MotorFL.write(0);
+  MotorFR.write(0);
+  MotorBL.write(0);
+  MotorBR.write(0);
+  delay(1000);
+  MotorFL.write(10);
+  MotorFR.write(10);
+  MotorBL.write(10);
+  MotorBR.write(10);
+  delay(60000);
+  MotorFL.write(0);
+  MotorFR.write(0);
+  MotorBL.write(0);
+  MotorBR.write(0);
+}
+
+void loop()
+{}
